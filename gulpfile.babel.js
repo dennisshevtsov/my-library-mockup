889b156c9fed898661188665f2d581a2224092f5
Dennis Shevtsov <dennisshevtsov@outlook.com>
2019-08-09 14:51:01 +0300

    colors; typography; grid

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,17 @@
-import { src, dest, } from 'gulp';
+import { src, dest, watch, } from 'gulp';
 import sass from 'gulp-sass';
 
 const scssDir = './scss';
 const cssDir = './css';
 
-const scssMask = `${scssDir}/styles.scss`;
+const scssMask = `${scssDir}/*.scss`;
+const stylesMask = `${scssDir}/styles.scss`;
 
-const buildScss = () => src(scssMask).
+const buildScss = () => src(stylesMask).
                         pipe(sass().on('error', sass.logError)).
                         pipe(dest(cssDir));
 
+const watchScss = () => watch(scssMask, buildScss);
+
 exports.default = buildScss;
+exports.watch = watchScss;
