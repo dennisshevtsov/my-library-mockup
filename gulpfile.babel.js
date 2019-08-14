242d7e219be1b1693b5227030475270304cb1f70
Dennis Shevtsov <dennisshevtsov@example.com>
2019-08-14 16:39:45 +0300

    fontawesome

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,14 @@
-import { src, dest, watch, } from 'gulp';
+import { src, dest, watch, series, } from 'gulp';
 import sass from 'gulp-sass';
 
 const scssDir = './scss';
 const cssDir = './css';
+const fontsSourceDir = './node_modules/@fortawesome/fontawesome-free/webfonts';
+const fontsDestDir = './webfonts';
 
 const scssMask = `${scssDir}/*.scss`;
 const stylesMask = `${scssDir}/styles.scss`;
+const fontsMask = `${fontsSourceDir}/*`;
 
 const buildScss = () => src(stylesMask).
                         pipe(sass().on('error', sass.logError)).
@@ -13,5 +16,9 @@ const buildScss = () => src(stylesMask).
 
 const watchScss = () => watch(scssMask, buildScss);
 
-exports.default = buildScss;
+const copyFonts = () => src(fontsMask).
+                        pipe(dest(fontsDestDir));
+
+exports.default = series(buildScss, copyFonts);
 exports.watch = watchScss;
+exports.copyFonts = copyFonts;
