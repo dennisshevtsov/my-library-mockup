2e08dfa4aef4f9dbb22022a46d47924440eea708
Dennis Shevtsov <dennis_shevtsov@example.com>
2019-08-19 14:45:56 +0300

    fix form margin; add build task; change icons font including

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,25 @@
 import { src, dest, watch, series, } from 'gulp';
 import sass from 'gulp-sass';
+import clean from 'gulp-clean-css';
+import concat from 'gulp-concat';
+
+const cssDirName = 'css';
+const fontDirName = 'fonts';
 
 const scssDir = './scss';
-const cssDir = './css';
+const cssDir = `./${cssDirName}`;
 const fontsSourceDir = './node_modules/@fortawesome/fontawesome-free/webfonts';
-const fontsDestDir = './webfonts';
+const fontsDestDir = `./${fontDirName}`;
+
+const prodDir = './prod';
+const prodCssDir = `${prodDir}/${cssDirName}`;
+const prodFontDir = `${prodDir}/${fontDirName}`;
+const prodCssFileName = 'styles.css';
 
 const scssMask = `${scssDir}/*.scss`;
+const cssMask = `${cssDir}/*.css`;
 const stylesMask = `${scssDir}/styles.scss`;
-const fontsMask = `${fontsSourceDir}/*`;
+const fontsMask = `${fontsSourceDir}/fa-solid-900.woff2`;
 
 const buildScss = () => src(stylesMask).
                         pipe(sass().on('error', sass.logError)).
@@ -19,6 +30,18 @@ const watchScss = () => watch(scssMask, buildScss);
 const copyFonts = () => src(fontsMask).
                         pipe(dest(fontsDestDir));
 
+const buildCssProd = () => src(cssMask).
+                           pipe(concat(prodCssFileName)).
+                           pipe(clean()).
+                           pipe(dest(prodCssDir));
+
+const copyFontsProd = () => src(fontsMask).
+                            pipe(dest(prodFontDir));
+
 exports.default = series(buildScss, copyFonts);
 exports.watch = watchScss;
 exports.copyFonts = copyFonts;
+
+exports.buildCssProd = buildCssProd;
+exports.copyFontsProd = copyFontsProd;
+exports.buildProd = series(buildCssProd, copyFontsProd);
